e664a94577ea42f6aa1cab0a661f0175d9348ccc
Chris Cooper <cjcooper252@gmail.com>
2024-10-25 10:42:13 -0700

    Infinite scroll clean up

--- src/routes/infinite-scroll.tsx
+++ src/routes/infinite-scroll.tsx
@@ -1,48 +1,70 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export const Route = createFileRoute("/infinite-scroll")({
   component: InfiniteScroll,
 });
 
+const LIMIT = 25;
+
 function InfiniteScroll() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [items, setItems] = useState<unknown[]>(Array.from({ length: 40 }));
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasMoreData, setHasMoreData] = useState(true);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [items, setItems] = useState<unknown[]>([]);
 
   const observer = useRef<IntersectionObserver>();
-  const lastItemRef = useRef(null);
 
-  const loadMoreData = () => {
+  const loadMoreData = async (pageNumber: number) => {
     setIsLoading(true);
-    setTimeout(() => {
-      setItems((prevItems) => [...prevItems, ...Array.from({ length: 40 })]);
-      setIsLoading(false);
-    }, 1000);
+
+    // Add a fake delay
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?_limit=${LIMIT}&_page=${pageNumber}`
+    );
+
+    const data = await response.json();
+
+    setItems((prevItems) => [...prevItems, ...data]);
+    setIsLoading(false);
+
+    if (data.length < LIMIT) {
+      setHasMoreData(false);
+    }
   };
 
+  // When page number changes, load more data
   useEffect(() => {
-    console.log("In use effect");
-    if (observer.current) observer.current.disconnect(); // Disconnect the old observer
+    loadMoreData(pageNumber);
+  }, [pageNumber]);
 
-    const options = {
-      root: null, // Observe viewport
-      rootMargin: "0px",
-      threshold: 1.0,
-    };
+  // useCallback caches the callback function, removing from the cache when isLoading or hasMoreData change
+  // By assigning to lastItemRef, the callback function is called every time the lastItemRef div re-rendered or component is mounted/unmounted
+  const lastItemRef = useCallback(
+    (node: HTMLDivElement | null) => {
+      if (isLoading) return;
 
-    observer.current = new IntersectionObserver((entries) => {
-      const target = entries[0];
-      if (target.isIntersecting) {
-        loadMoreData();
-      }
-    }, options);
+      if (observer.current) observer.current.disconnect(); // Disconnect the old observer
 
-    if (lastItemRef.current) {
-      observer.current.observe(lastItemRef.current);
-    }
+      const options = {
+        root: null, // Observe viewport
+        rootMargin: "0px",
+        threshold: 1.0,
+      };
 
-    return () => observer.current?.disconnect();
-  }, [lastItemRef.current]);
+      observer.current = new IntersectionObserver((entries) => {
+        const target = entries[0];
+        if (target.isIntersecting && hasMoreData) {
+          setPageNumber((prevState) => prevState + 1);
+        }
+      }, options);
+
+      if (node) observer.current.observe(node); // Observe the "last item ref" element
+    },
+    [isLoading, hasMoreData]
+  );
 
   return (
     <div className="p-2">
@@ -51,9 +73,9 @@ function InfiniteScroll() {
           <li key={index}>Item {index + 1}</li>
         ))}
       </ul>
-      <div className="bg-gray-200" ref={lastItemRef}>
-        {isLoading && "Loading more items..."}
-      </div>
+      {isLoading && <p> Loading more items...</p>}
+      <div className="bg-gray-200" ref={lastItemRef}></div>
+      {!hasMoreData && <p> No more results.</p>}
     </div>
   );
 }
