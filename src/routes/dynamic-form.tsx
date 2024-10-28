0af556bf3b00c99b02093753d343e4e195caa6fe
Chris Cooper <cjcooper252@gmail.com>
2024-10-28 15:33:15 -0700

    create renderField function

--- src/routes/dynamic-form.tsx
+++ src/routes/dynamic-form.tsx
@@ -22,68 +22,82 @@ export const Route = createFileRoute("/dynamic-form")({
 function DynamicForm() {
   console.log(data);
 
-  return (
-    <div className="p-2 flex flex-col gap-2">
-      {data.map((control) => {
-        // Radio
-        if (control.type === "radio") {
-          return (
-            <fieldset className="flex flex-col" key={control.id}>
-              <legend>{control.label}</legend>
-              {control.options?.map((option) => {
-                return (
-                  <div key={option.value}>
-                    <input
-                      type="radio"
-                      id={control.id + option.value}
-                      name={control.id}
-                      value={option.value}
-                    ></input>
-                    <label htmlFor={control.id + option.value}>
-                      {option.label}
-                    </label>
-                  </div>
-                );
-              })}
-            </fieldset>
-          );
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+  };
 
-          // Checkbox
-        } else if (control.type === "checkbox") {
-          return (
-            <fieldset key={control.id}>
-              <legend>{control.label}</legend>
+  function renderField(control: (typeof data)[number]) {
+    switch (control.type) {
+      case "radio":
+        return (
+          <fieldset className="flex flex-col" key={control.id}>
+            <legend>{control.label}</legend>
+            {control.options?.map((option) => {
+              return (
+                <div key={option.value}>
+                  <input
+                    type="radio"
+                    id={control.id + option.value}
+                    name={control.id}
+                    value={option.value}
+                  ></input>
+                  <label htmlFor={control.id + option.value}>
+                    {option.label}
+                  </label>
+                </div>
+              );
+            })}
+          </fieldset>
+        );
+      case "checkbox":
+        return (
+          <fieldset key={control.id}>
+            <legend>{control.label}</legend>
 
-              {control.options?.map((option) => {
-                return (
-                  <div key={option.value}>
-                    <input
-                      type="checkbox"
-                      id={control.id + option.value}
-                      name={control.id}
-                    />
-                    <label htmlFor={control.id + option.value}>
-                      {option.label}
-                    </label>
-                  </div>
-                );
-              })}
-            </fieldset>
-          );
-        } else if (control.type === "select") {
-          return (
-            <div className="flex flex-col" key={control.id}>
-              <label htmlFor={control.id}>{control.label}</label>
-              <select className="input" id={control.id}>
-                {control.options?.map((option) => (
-                  <option value={option.value}>{option.label}</option>
-                ))}
-              </select>
-            </div>
-          );
-        }
+            {control.options?.map((option) => {
+              return (
+                <div key={option.value}>
+                  <input
+                    type="checkbox"
+                    id={control.id + option.value}
+                    name={control.id}
+                  />
+                  <label htmlFor={control.id + option.value}>
+                    {option.label}
+                  </label>
+                </div>
+              );
+            })}
+          </fieldset>
+        );
+      case "select":
+        return (
+          <div className="flex flex-col" key={control.id}>
+            <label htmlFor={control.id}>{control.label}</label>
+            <select className="input" id={control.id}>
+              {control.options?.map((option) => (
+                <option value={option.value} key={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+          </div>
+        );
+      case "textarea":
+        return (
+          <div className="flex flex-col" key={control.id}>
+            <label htmlFor={control.id}>{control.label}</label>
+            <textarea
+              className="input"
+              id={control.id}
+              placeholder={control.placeholder}
+            />
+          </div>
+        );
 
-        // Text / email / number
+      case "text":
+      case "email":
+      case "number":
         return (
           <div className="flex flex-col" key={control.id}>
             <label htmlFor={control.id}>{control.label}</label>
@@ -95,7 +109,18 @@ function DynamicForm() {
             />
           </div>
         );
-      })}
-    </div>
+
+      default:
+        break;
+    }
+  }
+
+  return (
+    <form className="p-2 flex flex-col gap-2" onSubmit={handleSubmit}>
+      {data.map((control) => renderField(control))}
+      <button type="submit" className="button">
+        Submit
+      </button>
+    </form>
   );
 }
